87c105f351de646106b0640b97787a9f97ba5bb9
Tyrone Joel <125516735+Tyjay00@users.noreply.github.com>
2024-07-18 00:49:49 +0200

    Updated App.js
    Added a Home Button
    
    Implemented handleKeyDown to handle the Enter key press event in the search input field

--- App.js
+++ App.js
@@ -3,26 +3,68 @@ import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com?apikey=f4532191';
+// API URL with your API key
+const API_URL = 'https://www.omdbapi.com?apikey=f4532191';
 
 const App = () => {
+  // State to hold the search term entered by the user
   const [searchTerm, setSearchTerm] = useState("");
+  // State to hold the list of movies fetched from the API
   const [movies, setMovies] = useState([]);
+  // State to manage the dark mode toggle
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  // Fetch and display movies related to "Batman" when the component mounts
   useEffect(() => {
     searchMovies("Batman");
   }, []);
 
+  /**
+   * Function to search for movies based on the provided title.
+   * @param {string} title - The title of the movie to search for.
+   */
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
+    try {
+      const response = await fetch(`${API_URL}&s=${title}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+  
+      if (data.Response === 'True') {
+        setMovies(data.Search);
+      } else {
+        setMovies([]);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setMovies([]);
+    }
   };
 
+  /**
+   * Function to handle key press events.
+   * @param {object} event - The key press event.
+   */
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
+  };
+
+  /**
+   * Function to toggle the theme between light and dark mode.
+   */
   const toggleTheme = () => {
     setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  /**
+   * Function to reset the page to its initial state.
+   */
+  const resetPage = () => {
+    setSearchTerm("");
+    searchMovies("Batman");
   };
 
   return (
@@ -38,6 +80,7 @@ const App = () => {
         <input
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={handleKeyDown}
           placeholder="Search for movies"
         />
         <img
@@ -47,10 +90,12 @@ const App = () => {
         />
       </div>
 
+      <button className="home-button" onClick={resetPage}>Home</button>
+
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} />
+            <MovieCard key={movie.imdbID} movie={movie} />
           ))}
         </div>
       ) : (
